e19275f1031c33859fb461a4a6eb42bc4e2febc8
Redfact <tantelyrandria@ymail.com>
2020-05-28 19:23:02 +0200

    adding mails with button

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,34 @@
+require "faker"
+
 class EmailsController < ApplicationController
   def index
+    puts "Controller email"
+    @emails = Email.all
   end
+
+  def new
+    @email=Email.create(
+      object:Faker::Book.title,
+      body:Faker::Hipster.paragraph(sentence_count: 10),
+      read: [true,false].sample
+    )
+    
+    respond_to do |format|
+      format.html{ redirect_to :index}
+      format.js
+    end
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html{ redirect_to email_path(params[:id])}
+      format.js
+    end
+  end
+
+  def book_params
+    params.permit(:body, :object ,:read)
+  end
+
 end
